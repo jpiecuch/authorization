6e307e224cbec51e0466dab8e9c3d88fe9dba31d
Jakub Piecuch <jakub.piecuch@jakubpiecuch.pl>
2018-03-25 14:15:46 +0200

    Add eureka

--- src/main/java/pl/jakubpiecuch/authorization/feign/accounts/AccountsEndpoint.java
+++ src/main/java/pl/jakubpiecuch/authorization/feign/accounts/AccountsEndpoint.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "accountsEndpoint", url = "http://localhost:8081")
+@FeignClient(name = "accountsEndpoint", serviceId = "accounts")
 public interface AccountsEndpoint {
 
     @GetMapping(path = "/users/{name}")
