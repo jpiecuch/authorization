cf6a2f363d568b06bd1c4029d7090c749209854a
Jakub Piecuch <jakub.piecuch@jakubpiecuch.pl>
2018-04-08 19:25:45 +0200

    Add config clients

--- src/main/java/pl/jakubpiecuch/authorization/AuthorizationApplication.java
+++ src/main/java/pl/jakubpiecuch/authorization/AuthorizationApplication.java
@@ -2,10 +2,12 @@ package pl.jakubpiecuch.authorization;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableEurekaClient
 public class AuthorizationApplication {
 
     public static void main(String[] args) {
