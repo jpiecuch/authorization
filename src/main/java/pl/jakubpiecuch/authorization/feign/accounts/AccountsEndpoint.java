2c751708b85dd210c607ec3f087df7bc192ad296
jpiecuch <jakub.piecuch@jakubpiecuch.pl>
2018-09-03 14:09:11 +0200

    update kubernetes configs

--- src/main/java/pl/jakubpiecuch/authorization/feign/accounts/AccountsEndpoint.java
+++ src/main/java/pl/jakubpiecuch/authorization/feign/accounts/AccountsEndpoint.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "accountsEndpoint", serviceId = "accounts")
+@FeignClient(name = "accounts")
 public interface AccountsEndpoint {
 
     @GetMapping(path = "/users/{name}")
